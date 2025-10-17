c7d846423be506a4a5e195ac55162dfc5c2c2a64
arkn37 <arkn37.dev@outlook.com>
2025-10-16 20:13:55 -0600

    build: add hibernate envers and validator for audit

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,9 @@ val uuidCreatorVersion = "5.3.3"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.hibernate.validator:hibernate-validator")
     implementation("com.github.f4b6a3:uuid-creator:$uuidCreatorVersion")
+    implementation("org.hibernate.orm:hibernate-envers")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
