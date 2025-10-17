9c42e9e438f1431991aeb1f94bc1562a186aca71
arkn37 <arkn37.dev@outlook.com>
2025-10-16 20:12:54 -0600

    build: add uuid creator dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,12 @@ repositories {
     mavenCentral()
 }
 
+val uuidCreatorVersion = "5.3.3"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.github.f4b6a3:uuid-creator:$uuidCreatorVersion")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
