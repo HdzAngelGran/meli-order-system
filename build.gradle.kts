a5938c4a9db7aaeebc2baca41080a425d0a1f531
arkn37 <arkn37.dev@outlook.com>
2025-10-19 16:53:20 -0600

    fix: conflicts with other logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ val uuidCreatorVersion = "5.3.3"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.hibernate.validator:hibernate-validator")
     implementation("com.github.f4b6a3:uuid-creator:$uuidCreatorVersion")
